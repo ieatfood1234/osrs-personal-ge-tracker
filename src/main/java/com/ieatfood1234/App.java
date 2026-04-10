d2cccee358d3689683b0420b84e91971c8946751
ieatfood1234 <idkidcteehee@gmail.com>
2026-04-09 23:06:03 -0500

    V1: Simple GE Price Check
    - Creates and sends HTTP request to OSRS Wiki Real-Time Prices API
    - Utilize Jackson to parse raw JSON data sent from API
    - Takes item ID from user input
    - Displays High and Low prices in terminal

--- src/main/java/com/ieatfood1234/App.java
+++ src/main/java/com/ieatfood1234/App.java
@@ -1,13 +1,44 @@
 package com.ieatfood1234;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://prices.runescape.wiki/api/v1/osrs/latest"))
+                .header("User-Agent", "osrs-price-tracker/1.0 (discord: aether6765)")
+                .GET()
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, Object> jsonMap = mapper.readValue(response.body(), Map.class);
+        Map<String, Object> data = (Map<String, Object>) jsonMap.get("data");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the item id: ");
+        String itemId = scanner.nextLine();
+
+        if (data.containsKey(itemId)) {
+            Map<String, Object> itemData = (Map<String, Object>) data.get(itemId);
+            int highPrice = (int) itemData.get("high");
+            int lowPrice = (int) itemData.get("low");
+            System.out.println("High Price: " + highPrice);
+            System.out.println("Low Price: " + lowPrice);
+        }
+        else {
+            System.out.println("Item not found.");
+        }
     }
 }
